09044d0795f31bb5ea3fdd916c5086e92f4c3b24
Michael Friedrich <michael.friedrich@gmail.com>
2019-10-16 16:13:23 +0200

    Cleanup unused files

--- docker/start.sh
+++ docker/start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# ensure that dashing runs with utf8
+export LANG=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
 dashing start -p 8005
--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# ensure that dashing runs with utf8
-export LANG=en_US.UTF-8
-export LC_CTYPE=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-
-dashing start
